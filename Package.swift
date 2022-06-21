563a696176a09cfc0a5683a74c93385bda72583a
Ahmet Güvez <ahmetguvez03@hotmail.com>
2022-06-22 00:17:19 +0300

    error fix

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NetworkPackage",
-            dependencies: []),
+            dependencies: [],
+            path: "NetworkPackage"),
         .testTarget(
             name: "NetworkPackageTests",
             dependencies: ["NetworkPackage"]),
